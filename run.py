5f1e48ec9a9e6b8ce6a65d1b83036fb90c17539a
Виктор <vplotnikov16@yandex.ru>
2025-09-25 22:13:20 +0500

    Продолжил выстраивать структуру проекта. В конфиг добавил еще два параметра для БД. Добавил базовую модель таблицы Users (пока что user) для теста. Добавил flask-migrate в окружение, чтобы удобнее работать с миграциями в будущем.

--- run.py
+++ run.py
@@ -1,23 +1,11 @@
 from dotenv import load_dotenv
-from flask import Flask
 
-from app.config import Config
+from app import create_app
+from app.config import Config, EnvEnum
 
 load_dotenv()
 
-
-def create_app():
-    app = Flask(__name__)
-
-    app.config.from_object(Config)
-
-    @app.route('/')
-    def index_page():
-        return 'DEVELOPMENT' if app.config['DEBUG'] else 'PRODUCTION'
-
-    return app
-
+app = create_app(Config)
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(app.config['HOST'], app.config['PORT'])
+    app.run(debug=app.config[EnvEnum.debug])
