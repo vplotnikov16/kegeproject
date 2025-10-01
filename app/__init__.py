6fb076c6ffa084e5535d61d6b8fa17527cda07f4
vplotnikov16 <143165357+vplotnikov16@users.noreply.github.com>
2025-10-01 23:55:14 +0500

    auth: добавил регистрацию и авторизацию
    auth: добавил регистрацию и авторизацию

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
 from .config import Config
-from .extensions import db, migrate
+from .extensions import db, migrate, login_manager
+from .models import User
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -9,6 +11,21 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'pages.login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     from app import models
+
+    # регистрация blueprint'ов
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+
+    from app.routes.pages import pages_bp
+    app.register_blueprint(pages_bp, url_prefix='/')
 
     return app
---
+++ app/routes/pages.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+
+from app.models.user import User
+from app.extensions import db
+
+pages_bp = Blueprint("pages", __name__)
+
+
+@pages_bp.route('', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+
+@pages_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for("pages.index"))
+        return render_template("login.html", error="Неправильное имя пользователя и/или пароль")
+
+    return render_template("login.html")
+
+
+@pages_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('pages.index'))
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if User.query.filter_by(username=username).first():
+            error = "Пользователь с таким именем пользователя уже существует в системе"
+            return render_template("register.html", error=error)
+
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for("pages.index"))
+
+    return render_template("register.html")
+
+
+@pages_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('pages.index'))
+
+
+@pages_bp.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+
+@pages_bp.route('/stats')
+@login_required
+def stats():
+    return render_template('stats.html', user=current_user)
