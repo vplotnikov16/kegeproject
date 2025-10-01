8552f96719aae3a327a317662dcb818b8b9ae3cf
Эльмира <saitova-2004@mail.ru>
2025-10-01 23:55:14 +0500

    auth: добавил регистрацию и авторизацию
    auth: добавил регистрацию и авторизацию

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,18 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app.extensions import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
